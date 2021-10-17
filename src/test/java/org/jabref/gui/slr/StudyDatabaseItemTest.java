34aac8585df2b8d037e70770ac03c4781d1a0fb2
Gabriel Azevedo Batalha <batalha800@gmail.com>
2021-10-17 15:47:07 -0300

    Adicionados Testes
    Adicionados testes do método equals da classe StudyDatabaseItem

---
+++ src/test/java/org/jabref/gui/slr/StudyDatabaseItemTest.java
@@ -0,0 +1,56 @@
+package org.jabref.gui.slr;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudyDatabaseItemTest {
+
+    private StudyDatabaseItem item;
+
+    @BeforeEach
+    public void setup() {
+        item = new StudyDatabaseItem("Test", true);
+    }
+
+    @Test
+    public void equalsEqualTest() {
+        StudyDatabaseItem o = item;
+        assertTrue(item.equals(o));
+    }
+
+    @Test
+    public void equalsNullTest() {
+        StudyDatabaseItem o = null;
+        assertFalse(item.equals(o));
+    }
+
+    @Test
+    public void equalsOtherClassTest() {
+        Object o = new Object();
+        assertFalse(item.equals(o));
+    }
+
+    @Test
+    public void equalsEnabledTest() {
+        StudyDatabaseItem o = new StudyDatabaseItem("Test", false);
+        assertFalse(item.equals(o));
+    }
+
+    @Test
+    public void equalsNameTest() {
+        StudyDatabaseItem o = new StudyDatabaseItem("Testy", true);
+        assertFalse(item.equals(o));
+    }
+
+    @Test
+    public void equalsNoNameTest() {
+        StudyDatabaseItem o = new StudyDatabaseItem(null, true);
+        assertFalse(item.equals(o));
+    }
+
+}
